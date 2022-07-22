984ca4f4e315a9d3d616f07336cb4604c4e44e8b
Umar Chowdhury <umarfchy@gmail.com>
2022-07-23 00:20:39 +0600

    added basic routes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server Started at ${port}`));
+
+app.get("/", (_, res) => {
+  res.status(200).send({ message: "Welcome to demo app" });
+});
+
+app.get("/surprise", (_, res) => {
+  res.status(200).send({ message: "This is the surprise route" });
+});
+
+app.get("/dynamic/:id", (req, res) => {
+  res
+    .status(200)
+    .send({ message: "The route you specified is /dynamic" + req.params.id });
+});
+
+app.post("/headers", (req, res) => {
+  res.status(200).send(req.headers);
+});
+
+app.post("/body", (req, res) => res.status(200).send(req.body));
